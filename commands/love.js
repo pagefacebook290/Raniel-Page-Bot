0a56e92bbbe5e0cd2cde6fef8b2c673caa2e3571
Bogiti <https://github.com/geliemadason/Raniel-Page-Bot.git>
2024-12-09 07:00:47 +0800

    update love.js

--- commands/love.js
+++ commands/love.js
@@ -3,16 +3,16 @@ const axios = require('axios');
 const { sendMessage } = require('../handles/sendMessage');
 
 module.exports = {
-  name: 'love',
+  name: 'aimusic',
   description: 'Interact with AI Girlfriend',
   usage: 'love [your message]',
   author: 'coffee',
   async execute(senderId, args, pageAccessToken) {
     const prompt = args.join(' ');
-    if (!prompt) return sendMessage(senderId, { text: "Usage: ai-gf <your message>" }, pageAccessToken);
+    if (!prompt) return sendMessage(senderId, { text: "Usage: aimusic <your message>" }, pageAccessToken);
 
     try {
-      const { data } = await axios.get(`https://api.joshweb.click/api/ai-gf?q=${encodeURIComponent(prompt)}`);
+      const { data } = await axios.get(`https://api.joshweb.click/api/aimusic?prompt=${encodeURIComponent(prompt)}`);
       sendMessage(senderId, { text: data.response }, pageAccessToken);
     } catch {
       sendMessage(senderId, { text: 'Error generating response. Try again later.' }, pageAccessToken);
