1f36c6fcd0f6f97110a6266bd2b906ce97df40d5
Bogiti <https://github.com/geliemadason/Raniel-Page-Bot.git>
2024-12-11 17:44:34 +0800

    update uid.js

--- commands/uid.js
+++ commands/uid.js
@@ -1,19 +1,4 @@
 
-const { sendMessage } = require('../handles/sendMessage');
-
-module.exports = {
-  name: 'uid',
-  description: 'Get user ID',
-  usage: 'uid',
-  author: 'Name',
-  category: 'Info',
-  async execute(senderID, args, pageAccessToken) {
-    // Assuming senderID is already in the correct format
-    await sendMessage(senderID, {
-      text: `Your user ID is: ${senderID}`
-    }, pageAccessToken);
-  }
-};
 
 const { sendMessage } = require('../handles/sendMessage');
 
