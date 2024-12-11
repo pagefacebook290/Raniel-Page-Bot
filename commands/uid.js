a8bae9672001f4686312e60cc2a5a22e6dee6425
Bogiti <https://github.com/geliemadason/Raniel-Page-Bot.git>
2024-12-11 14:04:16 +0800

    update

--- commands/uid.js
+++ commands/uid.js
@@ -1,21 +1,14 @@
-const axios = require('axios');
 const { sendMessage } = require('../handles/sendMessage');
 
 module.exports = {
-  name: "uid",
-  execute: async ({ api, event, pageAccessToken }) => {
-    if (Object.keys(event.mentions).length === 0) {
-      if (event.messageReply) {
-        const senderID = event.messageReply.senderID;
-        api.sendMessage(`User ID: ${senderID}`, event.threadID, { token: pageAccessToken });
-      } else {
-        api.sendMessage(`Your User ID: ${event.senderID}`, event.threadID, { token: pageAccessToken });
-      }
-    } else {
-      for (const mentionID in event.mentions) {
-        const mentionName = event.mentions[mentionID];
-        api.sendMessage(`User ID of ${mentionName.replace('@', '')}: ${mentionID}`, event.threadID, { token: pageAccessToken });
-      }
-    }
+  name: 'uid',
+  description: 'Get user ID',
+  usage: 'uid',
+  author: 'Name',
+  category: 'Info',
+  async execute(senderId, args, pageAccessToken) {
+    await sendMessage(senderId, {
+      text: `Your user ID is: ${senderId}`
+    }, pageAccessToken);
   }
 };
