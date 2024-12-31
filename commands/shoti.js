f280db0c3b0cb5a031ce386f5a7e917bd7c49838
Bogiti <https://github.com/geliemadason/Raniel-Page-Bot.git>
2024-12-31 15:40:33 +0800

    update

--- commands/shoti.js
+++ commands/shoti.js
@@ -1,21 +1,23 @@
+
 const axios = require('axios');
 const { sendMessage } = require('../handles/sendMessage');
 
 module.exports = {
-name: 'shoti',
-usage: 'shoti',
-description: 'Generate a random TikTok video.',
+name: 'eabab',
+usage: 'eabab',
+description: 'Generate a random girl video.',
 author: 'Jerome',
 async execute(senderId, args, pageAccessToken, sendMessage) {
 try {
-const apiUrl = 'https://betadash-shoti-yazky.vercel.app/shotizxx?apikey=shipazu';
+const apiUrl = 'https://hiroshi-api.onrender.com/video/eabab';
 const response = await axios.get(apiUrl);
-const videoUrl = response.data.videoDownloadLink;
-//const title = response.data.title;
-//const username = response.data.username;
-//const tiktokUrl = response.data.tiktokUrl;
+const videoUrl = response.data.link;
+const title = response.data.title;
+const username = response.data.username;
+const displayname = response.data.displayname;
 
-  const message = `Sending...`;
+  const message = `Title: ${title}\nUsername: ${username}\nDisplay Name: ${displayname}\n\n𝕯𝖔𝖜𝖓𝖑𝖔𝖆𝖉𝖎𝖓𝖌 𝕻𝖑𝖊𝖆𝖘𝖊 𝖂𝖆𝖎𝖙...
+`;
   await sendMessage(senderId, { text: message }, pageAccessToken);
 
   const videoMessage = {
