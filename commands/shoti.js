65c476ba095d9174fc77b172e8e249002b4fd79b
Bogiti <https://github.com/geliemadason/Raniel-Page-Bot.git>
2025-01-04 06:14:12 +0800

    update shoti.js

--- commands/shoti.js
+++ commands/shoti.js
@@ -1,27 +1,25 @@
- const axios = require('axios');
+;
+const axios = require('axios');
 const { sendMessage } = require('../handles/sendMessage');
 
 module.exports = {
-  name: 'shoti',
-  description: 'Download short video.',
-  usage: 'shoti',
-  author: 'Iyo',
+  name: 'eabab',
+  usage: 'eabab',
+  description: 'Generate a random girl video.',
+  author: 'Jerome',
+  
   async execute(senderId, args, pageAccessToken, sendMessage) {
     try {
-      const apiUrl = 'https://kaiz-apis.gleeze.com/api/shoti';
+      const apiUrl = 'https://kaiz-apis.gleeze.com/api/shoti'; // Updated API URL
       const response = await axios.get(apiUrl);
-      
-      if (!response.data) {
-        throw new Error('No data found');
-      }
-      
-      const videoUrl = response.data[0].url;
-      const title = response.data[0].title;
-      
-      const message = `Title: ${title}\nDownloading...`;
-      
+      const videoUrl = response.data.link; // Assuming the response structure is similar
+      const title = response.data.title;
+      const username = response.data.username;
+      const displayname = response.data.displayname;
+
+      const message = `Title: ${title}\nUsername: ${username}\nDisplay Name: ${displayname}\n\n𝕯𝖔𝖜𝖓𝖑𝖔𝖆𝖉𝖎𝖓𝖌 𝕻𝖑𝖊𝖆𝖘𝖊 𝖂𝖆𝖎𝖙...`;
       await sendMessage(senderId, { text: message }, pageAccessToken);
-      
+
       const videoMessage = {
         attachment: {
           type: 'video',
@@ -30,11 +28,12 @@ module.exports = {
           },
         },
       };
-      
       await sendMessage(senderId, videoMessage, pageAccessToken);
     } catch (error) {
       console.error('Error:', error.message);
-      sendMessage(senderId, { text: 'Sorry, may error sa pag-download ng video.' }, pageAccessToken);
+      await sendMessage(senderId, {
+        text: 'Sorry, there was an error generating the video. Please try again later.',
+      }, pageAccessToken);
     }
   },
 };
