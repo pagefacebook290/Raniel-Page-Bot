021026089501d6642e6fbaf968be863091b0543f
Bogiti <https://github.com/geliemadason/Raniel-Page-Bot.git>
2025-01-18 10:14:04 +0800

    update uid.js

--- commands/uid.js
+++ commands/uid.js
@@ -2,19 +2,24 @@ const { sendMessage } = require('../handles/sendMessage');
 
 module.exports = {
   name: 'uid',
-  description: 'Get user ID',
-  usage: 'uid',
+  description: 'Get Facebook user ID',
+  usage: 'uid [Facebook profile URL]',
   author: 'Name',
   category: 'Info',
   async execute(senderID, args, pageAccessToken) {
-    try {
-      const userId = String(senderID);
-      if (!userId) throw new Error('Invalid sender ID');
-      
-      await sendMessage(senderID, { text: `Ang iyong user ID ay: ${userId}` }, pageAccessToken);
-    } catch (error) {
-      console.error('Error:', error.message);
-      await sendMessage(senderID, { text: 'May error sa pagkuha ng user ID.' }, pageAccessToken);
+    if (!args.length) {
+      return await sendMessage(senderID, { text: 'Please provide Facebook profile URL.' }, pageAccessToken);
     }
+
+    const url = args[0];
+    const regex = /(?:id=|\/)(\d+)/;
+    const match = url.match(regex);
+
+    if (!match) {
+      return await sendMessage(senderID, { text: 'Invalid Facebook profile URL.' }, pageAccessToken);
+    }
+
+    const userId = match[1];
+    await sendMessage(senderID, { text: `Ang Facebook user ID ay: ${userId}` }, pageAccessToken);
   }
 };
