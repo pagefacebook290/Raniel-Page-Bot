19c034a69673495017f0813c3b938aadfaef3e03
Bogiti <https://github.com/geliemadason/Raniel-Page-Bot.git>
2025-01-18 10:42:58 +0800

    update uid.js

--- commands/uid.js
+++ commands/uid.js
@@ -1,27 +1,16 @@
-const axios = require('axios');
 const { sendMessage } = require('../handles/sendMessage');
 
 module.exports = {
   name: 'uid',
-  description: 'Get Facebook user ID',
-  usage: 'uid [Facebook profile URL]',
+  description: 'Get user ID',
+  usage: 'uid',
   author: 'Name',
   category: 'Info',
   async execute(senderID, args, pageAccessToken) {
-    if (!args.length) {
-      return await sendMessage(senderID, { text: 'Please provide Facebook profile URL.' }, pageAccessToken);
-    }
-
-    const url = args[0];
-    const username = url.split('/').pop();
-
-    try {
-      const response = await axios.get(`https://graph.facebook.com/${username}?fields=id`);
-      const userId = response.data.id;
-      await sendMessage(senderID, { text: `Ang Facebook user ID ay: ${userId}` }, pageAccessToken);
-    } catch (error) {
-      console.error('Error:', error.message);
-      await sendMessage(senderID, { text: 'Error sa pagkuha ng user ID.' }, pageAccessToken);
-    }
+    // Convert senderID to string if necessary
+    const userId = String(senderID);
+    await sendMessage(senderID, {
+      text: `Your user ID is oten ka: ${userId}`
+    }, pageAccessToken);
   }
 };
