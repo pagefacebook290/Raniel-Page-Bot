1d895d812e184924fa10ac6ca7ab38fd41e6ec95
Bogiti <https://github.com/geliemadason/Raniel-Page-Bot.git>
2025-01-18 10:03:30 +0800

    update uid.js

--- commands/uid.js
+++ commands/uid.js
@@ -1,37 +1,20 @@
-const { MessengerBot } = require('messenger-bot');
 const { sendMessage } = require('../handles/sendMessage');
-const bot = new MessengerBot(pageAccessToken);
 
-const uidCommand = async (senderId, data, pageAccessToken) => {
-  const threadId = data.thread_id;
-  let senderId;
-
-  if (!data.reply) {
-    senderId = data.author_id;
-  } else {
-    senderId = data.reply.author;
-  }
-
-  try {
-    await bot.shareContact(`${senderId}`, senderId, threadId);
-  } catch (error) {
-    console.error('Error:', error.message);
-    sendMessage(senderId, { text: 'Error sharing contact.' }, pageAccessToken);
-  }
-};
-
-const config = {
+module.exports = {
   name: 'uid',
-  description: 'Share user ID as contact.',
+  description: 'Get user ID',
   usage: 'uid',
-  author: 'Your Name',
-  async execute(senderId, args, pageAccessToken) {
-    const data = {
-      thread_id: senderId,
-      author_id: senderId,
-      reply: args.reply
-    };
-
-    await uidCommand(senderId, data, pageAccessToken);
+  author: 'Name',
+  category: 'Info',
+  async execute(senderID, args, pageAccessToken) {
+    try {
+      const userId = String(senderID);
+      if (!userId) throw new Error('Invalid sender ID');
+      
+      await sendMessage(senderID, { text: `Ang iyong user ID ay: ${userId}` }, pageAccessToken);
+    } catch (error) {
+      console.error('Error:', error.message);
+      await sendMessage(senderID, { text: 'May error sa pagkuha ng user ID.' }, pageAccessToken);
+    }
   }
 };
