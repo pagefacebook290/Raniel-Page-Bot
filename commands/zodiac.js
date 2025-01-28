68fa3ed1d62127a67f9dc23c2b4b1b4e9693999a
Bogiti <https://github.com/geliemadason/Raniel-Page-Bot.git>
2025-01-28 10:04:34 +0800

    update zodiac.js

--- commands/zodiac.js
+++ commands/zodiac.js
@@ -212,7 +212,7 @@ const { sendMessage } = require('../handles/sendMessage');
     return sendMessage(senderId, { text: `Sorry, I don't have information about the zodiac sign for ${month}.` }, pageAccessToken);
   }
   const fact = zodiacFacts[month];
-  const message = `Dates: ${fact.dates}\n\n${fact.fact}\n\nZodiac: ${fact.zodiac}\nSymbol: ${fact.symbol}\nElements: ${fact.element}\nRuling Planet: ${fact.ruling_planet}\n\nPersonality Traits:\n${fact.personality_traits.join(', ')}\n\nBest Matches: ${fact.best_matches.join(', ')}\n\nWorst Matches: ${fact.worst_matches.join(', ')}`;
+  const message = `Dates: ${fact.dates}\n\nZodiac: ${fact.zodiac}\nSymbol: ${fact.symbol}\nElements: ${fact.element}\nRuling Planet: ${fact.ruling_planet}\n\nFacts: ${fact.fact}\n\nPersonality Traits:\n${fact.personality_traits.join(', ')}\n\nBest Matches: ${fact.best_matches.join(', ')}\n\nWorst Matches: ${fact.worst_matches.join(', ')}`;
 sendMessage(senderId, { text: message }, pageAccessToken);
 }
 };
