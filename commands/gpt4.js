32c570cbb511475836726bc545fdcfa40c0b8837
Bogiti <https://github.com/geliemadason/Raniel-Page-Bot.git>
2025-02-02 09:25:46 +0800

    update gpt4.js

--- commands/gpt4.js
+++ commands/gpt4.js
@@ -6,27 +6,23 @@ module.exports = {
   description: 'Interact with GPT-4o',
   usage: 'gpt4 [your message]',
   author: 'coffee',
-
   async execute(senderId, args, pageAccessToken) {
     const prompt = args.join(' ');
     if (!prompt) return sendMessage(senderId, { text: "Usage: gpt4 <question>" }, pageAccessToken);
 
     try {
-      const { data: { response } } = await axios.get(`api/deepseek?prompt=${encodeURIComponent(prompt)}&uid=${senderId}`);
-
+      const img = 'https://www.thispersondoesnotexist.com/'; // default image URL
+      const { data: { response } } = await axios.get(`api/deepseek?prompt=${encodeURIComponent(prompt)}&uid=${senderId}&img=${encodeURIComponent(img)}`);
       const parts = [];
-
       for (let i = 0; i < response.length; i += 1999) {
         parts.push(response.substring(i, i + 1999));
       }
-
       // send all msg parts
       for (const part of parts) {
         await sendMessage(senderId, { text: part }, pageAccessToken, 'kaon ka tae?');
       }
-
     } catch {
-      sendMessage(senderId, { text: 'There was an error generating the content. Please try again later.' }, pageAccessToken);
+      sendMessage(senderId, { text: 'Mayroong error sa paggawa ng nilalaman. Pakiusap subukang muli mamaya.' }, pageAccessToken);
     }
   }
-}; 
+};
