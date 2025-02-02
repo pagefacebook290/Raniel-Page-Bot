4755cd2dc795169983264e8565dcee2625bf6b49
Bogiti <https://github.com/geliemadason/Raniel-Page-Bot.git>
2025-02-02 12:07:58 +0800

    update gpt4.js

--- commands/gpt4.js
+++ commands/gpt4.js
@@ -1,18 +1,18 @@
 const axios = require('axios');
-const { sendmessage } = require('./handles/sendmessage');
+const { sendMessage } = require('../handles/sendMessage');
 
 module.exports = {
   name: 'gpt4',
-  description: 'interact with gpt-4o',
+  description: 'Interact with GPT-4o',
   usage: 'gpt4 [your message]',
   author: 'coffee',
 
-  async execute(senderid, args, pageaccesstoken) {
+  async execute(senderId, args, pageAccessToken) {
     const prompt = args.join(' ');
-    if (!prompt) return sendmessage(senderid, { text: "usage: gpt4 <question>" }, pageaccesstoken);
+    if (!prompt) return sendMessage(senderId, { text: "Usage: gpt4 <question>" }, pageAccessToken);
 
     try {
-      const { data: { response } } = await axios.get(`api/4ov2?prompt=${encodeURIComponent(prompt)}&uid=${senderid}&img=https%3A%2F%2Fwww.thispersondoesnotexist.com%2F`);
+      const { data: { response } } = await axios.get(`https://zaikyoo-api.onrender.com/api/4ov2?prompt=${prompt}&uid=${senderId}`);
 
       const parts = [];
 
@@ -22,11 +22,11 @@ module.exports = {
 
       // send all msg parts
       for (const part of parts) {
-        await sendmessage(senderid, { text: part }, pageaccesstoken);
+        await sendMessage(senderId, { text: part }, pageAccessToken, 'kaon ka tae?');
       }
 
-    } catch (error) {
-      sendmessage(senderid, { text: 'there was an error generating the content. please try again later.' }, pageaccesstoken);
+    } catch {
+      sendMessage(senderId, { text: 'There was an error generating the content. Please try again later.' }, pageAccessToken);
     }
   }
 };
