8adbc5db0789004f6de546604d35102852cf4218
Bogiti <https://github.com/geliemadason/Raniel-Page-Bot.git>
2025-02-02 08:51:33 +0800

    update gpt4.js

--- commands/gpt4.js
+++ commands/gpt4.js
@@ -1,4 +1,5 @@
- const axios = require('axios');
+
+const axios = require('axios');
 const { sendMessage } = require('../handles/sendMessage');
 
 module.exports = {
@@ -6,25 +7,20 @@ module.exports = {
   description: 'Interact with GPT-4o',
   usage: 'gpt4 [your message]',
   author: 'coffee',
-
   async execute(senderId, args, pageAccessToken) {
     const prompt = args.join(' ');
     if (!prompt) return sendMessage(senderId, { text: "Usage: gpt4 <question>" }, pageAccessToken);
 
     try {
-      const { data: { response } } = await axios.get(`http://zaikyoo.onrender.com/docs?fbclid=${encodeURIComponent(prompt)}&uid=${senderId}`);
-
+      const { data: { response } } = await axios.get(`api/4ov2?prompt=hi what is this image&uid=${senderId}&img=https%3A%2F%2Fwww.thispersondoesnotexist.com%2F`);
       const parts = [];
-
       for (let i = 0; i < response.length; i += 1999) {
         parts.push(response.substring(i, i + 1999));
       }
-
       // send all msg parts
       for (const part of parts) {
         await sendMessage(senderId, { text: part }, pageAccessToken, 'kaon ka tae?');
       }
-
     } catch {
       sendMessage(senderId, { text: 'There was an error generating the content. Please try again later.' }, pageAccessToken);
     }
