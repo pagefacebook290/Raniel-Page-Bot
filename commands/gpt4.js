4ee3249422d6d23bce630d8d5c728ec4c5c7b5f8
Bogiti <https://github.com/geliemadason/Raniel-Page-Bot.git>
2025-02-02 11:28:02 +0800

    update gpt4.js

--- commands/gpt4.js
+++ commands/gpt4.js
@@ -2,27 +2,26 @@ const axios = require('axios');
 const { sendMessage } = require('../handles/sendMessage');
 
 module.exports = {
-  name: 'gpt4',
-  description: 'Interact with GPT-4o',
-  usage: 'gpt4 [your message]',
-  author: 'coffee',
-  async execute(senderId, args, pageAccessToken) {
-    const prompt = args.join(' ');
-    if (!prompt) return sendMessage(senderId, { text: "Usage: gpt4 <question>" }, pageAccessToken);
+name: 'gpt4',
+description: 'Interact with GPT-4o',
+usage: 'gpt4 [your message]',
+author: 'coffee',
+async execute(senderId, args, pageAccessToken) {
+const prompt = args.join(' ');
+if (!prompt) return sendMessage(senderId, { text: "Usage: gpt4 <question>" }, pageAccessToken);
 
-    try {
-      const img = 'https://www.thispersondoesnotexist.com/'; // default image URL
-      const { data: { response } } = await axios.get(`api/deepseek?prompt=${encodeURIComponent(prompt)}&uid=${senderId}&img=${encodeURIComponent(img)}`);
-      const parts = [];
-      for (let i = 0; i < response.length; i += 1999) {
-        parts.push(response.substring(i, i + 1999));
-      }
-      // send all msg parts
-      for (const part of parts) {
-        await sendMessage(senderId, { text: part }, pageAccessToken, 'kaon ka tae?');
-      }
-    } catch {
-      sendMessage(senderId, { text: 'Mayroong error sa paggawa ng nilalaman. Pakiusap subukang muli mamaya.' }, pageAccessToken);
-    }
+try {
+  const { data: { response } } = await axios.get(`api/4ov2?prompt=${encodeURIComponent(prompt)}&uid=${senderId}&img=https%3A%2F%2Fwww.thispersondoesnotexist.com%2F`);
+  const parts = [];
+  for (let i = 0; i < response.length; i += 1999) {
+    parts.push(response.substring(i, i + 1999));
   }
+  // send all msg parts
+  for (const part of parts) {
+    await sendMessage(senderId, { text: part }, pageAccessToken, 'kaon ka tae?');
+  }
+} catch {
+  sendMessage(senderId, { text: 'Mayroong error sa paggawa ng nilalaman. Pakiusap subukang muli mamaya.' }, pageAccessToken);
+}
+}
 };
