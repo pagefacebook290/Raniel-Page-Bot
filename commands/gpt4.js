8ec143774bb5cd22ee7cf0373195d14459e17094
Bogiti <https://github.com/geliemadason/Raniel-Page-Bot.git>
2025-02-02 11:34:08 +0800

    update gpt4.js

--- commands/gpt4.js
+++ commands/gpt4.js
@@ -1,27 +1,32 @@
 const axios = require('axios');
-const { sendMessage } = require('../handles/sendMessage');
+const { sendmessage } = require('../handles/sendmessage');
 
 module.exports = {
-name: 'gpt4',
-description: 'Interact with GPT-4o',
-usage: 'gpt4 [your message]',
-author: 'coffee',
-async execute(senderId, args, pageAccessToken) {
-const prompt = args.join(' ');
-if (!prompt) return sendMessage(senderId, { text: "Usage: gpt4 <question>" }, pageAccessToken);
+  name: 'gpt4',
+  description: 'interact with gpt-4o',
+  usage: 'gpt4 [your message]',
+  author: 'coffee',
 
-try {
-  const { data: { response } } = await axios.get(`api/4ov2?prompt=${encodeURIComponent(prompt)}&uid=${senderId}&img=https%3A%2F%2Fwww.thispersondoesnotexist.com%2F`);
-  const parts = [];
-  for (let i = 0; i < response.length; i += 1999) {
-    parts.push(response.substring(i, i + 1999));
+  async execute(senderid, args, pageaccesstoken) {
+    const prompt = args.join(' ');
+    if (!prompt) return sendmessage(senderid, { text: "usage: gpt4 <question>" }, pageaccesstoken);
+
+    try {
+      const { data: { response } } = await axios.get(`api/4ov2?prompt=${encodeURIComponent(prompt)}&uid=${senderid}&img=https%3A%2F%2Fwww.thispersondoesnotexist.com%2F`);
+
+      const parts = [];
+
+      for (let i = 0; i < response.length; i += 1999) {
+        parts.push(response.substring(i, i + 1999));
+      }
+
+      // send all msg parts
+      for (const part of parts) {
+        await sendmessage(senderid, { text: part }, pageaccesstoken);
+      }
+
+    } catch (error) {
+      sendmessage(senderid, { text: 'there was an error generating the content. please try again later.' }, pageaccesstoken);
+    }
   }
-  // send all msg parts
-  for (const part of parts) {
-    await sendMessage(senderId, { text: part }, pageAccessToken, 'kaon ka tae?');
-  }
-} catch {
-  sendMessage(senderId, { text: 'Mayroong error sa paggawa ng nilalaman. Pakiusap subukang muli mamaya.' }, pageAccessToken);
-}
-}
 };
