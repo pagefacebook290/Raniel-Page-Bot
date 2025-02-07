53124bce1265433cb208a70f5becdd971a56827d
Bogiti <https://github.com/geliemadason/Raniel-Page-Bot.git>
2025-02-07 18:40:36 +0800

    update

--- commands/ai.js
+++ commands/ai.js
@@ -2,30 +2,30 @@ const axios = require('axios');
 const { sendMessage } = require('../handles/sendMessage');
 
 module.exports = {
-  name: 'ai',
+  name: 'gpt4',
   description: 'Interact with AI',
-  usage: 'ai [your message]',
+  usage: 'gpt4 [your message]',
   author: 'mark',
   async execute(senderId, args, pageAccessToken) {
     const prompt = args.join(' ');
-    
+
     if (!prompt) {
-      return sendMessage(senderId, { 
-        text: `━━━━━━━━━━━━━\nUsage: ai [message]\n━━━━━━━━━━━━━━` 
+      return sendMessage(senderId, {
+        text: `━━━━━━━━━━━━━\nUsage: ai [message]\n━━━━━━━━━━━━━━`
       }, pageAccessToken);
     }
 
     try {
       const apiUrl = `https://api.shizuki.linkpc.net/api/groq?ask=${encodeURIComponent(prompt)}&uid=${senderId}`;
       const { data: { response } } = await axios.get(apiUrl);
-      
-      await sendMessage(senderId, { 
-        text: `━━━━━━━━━━━━━\n${response}\n━━━━━━━━━━━━━` 
+
+      await sendMessage(senderId, {
+        text: `━━━━━━━━━━━━━\n${response}\n━━━━━━━━━━━━━`
       }, pageAccessToken);
 
     } catch (error) {
-      await sendMessage(senderId, { 
-        text: `━━━━━━━━━━━━━\nError! Try again.\n━━━━━━━━━━━━` 
+      await sendMessage(senderId, {
+        text: `━━━━━━━━━━━━━\nError! Try again.\n━━━━━━━━━━━━`
       }, pageAccessToken);
     }
   }
---
+++ gpt4.js
@@ -0,0 +1,34 @@
+ const axios = require('axios');
+const { sendMessage } = require('../handles/sendMessage');
+
+module.exports = {
+  name: 'gpt4',
+  description: 'Interact with GPT-4o',
+  usage: 'gpt4 [your message]',
+  author: 'coffee',
+
+  async execute(senderId, args, pageAccessToken) {
+    const prompt = args.join(' ');
+    if (!prompt) return sendMessage(senderId, { text: "Usage: gpt4 <question>" }, pageAccessToken);
+
+    try {
+      const { data: { response } } = await axios.get(`https://kaiz-apis.gleeze.com/api/gpt-4o?ask=${encodeURIComponent(prompt)}&uid=${senderId}&webSearch=off`);
+
+      const parts = [];
+
+      for (let i = 0; i < response.length; i += 1999) {
+        parts.push(response.substring(i, i + 1999));
+      }
+
+      // send all msg parts
+      for (const part of parts) {
+        await sendMessage(senderId, { text: part }, pageAccessToken, 'kaon ka tae?');
+      }
+
+    } catch {
+      sendMessage(senderId, { text: 'There was an error generating the content. Please try again later.' }, pageAccessToken);
+    }
+  }
+};
+
+ 
