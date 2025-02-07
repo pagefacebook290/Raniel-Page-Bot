382748c8531044b7165565feea2d19dfdde2aa2e
Bogiti <https://github.com/geliemadason/Raniel-Page-Bot.git>
2025-02-08 06:02:33 +0800

    update ai.js

--- commands/ai.js
+++ commands/ai.js
@@ -2,9 +2,9 @@ const axios = require('axios');
 const { sendMessage } = require('../handles/sendMessage');
 
 module.exports = {
-  name: 'gpt4',
+  name: 'ai',
   description: 'Interact with AI',
-  usage: 'gpt4 [your message]',
+  usage: 'ai [your message]',
   author: 'mark',
   async execute(senderId, args, pageAccessToken) {
     const prompt = args.join(' ');
@@ -20,7 +20,7 @@ module.exports = {
       const { data: { response } } = await axios.get(apiUrl);
 
       await sendMessage(senderId, {
-        text: `━━━━━━━━━━━━━\n${response}\n━━━━━━━━━━━━━`
+        text: `━━━━━━━━━━━━━\n${response}\n━━━━━━━━━━━━━\nThis Ai is made by ICT students in Pau Excellencia Global Academy Foundation, Inc.(Pegafi)\n━━━━━━━━━━━━━━━━━━\nAdmin Link: https://www.facebook.com/100092248658233/`
       }, pageAccessToken);
 
     } catch (error) {
