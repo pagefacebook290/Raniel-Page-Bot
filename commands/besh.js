3bd1c3767b72d0f6aef6e52c00f2d0de6338597d
Bogiti <https://github.com/geliemadason/Raniel-Page-Bot.git>
2025-02-13 13:19:12 +0800

    update besh.js

--- commands/besh.js
+++ commands/besh.js
@@ -3,21 +3,17 @@ const { sendMessage } = require('../handles/sendMessage');
 
 module.exports = {
   name: 'besh',
-  description: 'Get answer from unity ai',
-  usage: 'unity [question]',
+  description: 'Get haiku from API',
+  usage: 'besh <prompt>',
   author: 'raniel',
-
   async execute(senderId, args, pageAccessToken) {
-
     const prompt = args.join(' ');
     if (!prompt) return sendMessage(senderId, { text: "Usage: besh <prompt>" }, pageAccessToken);
-
     try {
-       const { data } = await axios.get(`https://markdevs-last-api-p2y6.onrender.com/ashley?prompt=${encodeURIComponent(prompt)}&uid=1`);
-      
+      const { data } = await axios.get(`https://zaikyoo.onrender.com/api/claude3-5-haiku?prompt=${encodeURIComponent(prompt)}&uid=${senderId}`);
       sendMessage(senderId, { text: data.response }, pageAccessToken);
     } catch {
-      sendMessage(senderId, { text: 'There was an error generating the content. Please try again later.' }, pageAccessToken);
+      sendMessage(senderId, { text: 'There was an error generating the haiku. Please try again later.' }, pageAccessToken);
     }
   }
 };
