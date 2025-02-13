b59758eeb11c57666ef1d68c06d432d2c15398bf
Bogiti <https://github.com/geliemadason/Raniel-Page-Bot.git>
2025-02-13 23:05:57 +0800

    update fbdown.js

--- commands/fbdown.js
+++ commands/fbdown.js
@@ -18,13 +18,14 @@ module.exports = {
 
     try {
       const response = await axios.get(`https://kaiz-apis.gleeze.com/api/fbdl?url=${encodeURIComponent(videoLink)}`);
-      const videoData = JSON.stringify(response.data);
+      const videoData = response.data;
+      const videoUrl = videoData.videoUrl;
 
-      if (!videoData) {
+      if (!videoUrl) {
         return sendMessage(senderId, { text: 'Failed to retrieve video.' }, pageAccessToken);
       }
 
-      sendMessage(senderId, { text: 'Video data: ' + videoData }, pageAccessToken);
+      sendMessage(senderId, { attachment: { type: 'video', payload: { url: videoUrl } } }, pageAccessToken);
     } catch (error) {
       console.error('Error:', error.message);
       sendMessage(senderId, { text: 'An error occurred. Try again later.' }, pageAccessToken);
