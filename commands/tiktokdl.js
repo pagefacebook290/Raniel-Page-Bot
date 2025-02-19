7de4adf8b5bb399665521f4335d8d73be2270079
Bogiti <https://github.com/geliemadason/Raniel-Page-Bot.git>
2025-02-19 18:08:55 +0800

    update tiktokdl.js

--- commands/tiktokdl.js
+++ commands/tiktokdl.js
@@ -3,37 +3,28 @@ const { sendMessage } = require('../handles/sendMessage');
 const fs = require('fs');
 const token = fs.readFileSync('token.txt', 'utf8');
 
-const tiktokApi = 'https://sandipbaru.com/tiktok?url=';
-
-const getTikTokVideo = async (url) => {
-  try {
-    const response = await axios.get(`${tiktokApi}${url}`);
-    const videoUrl = response.data;
-    return videoUrl;
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-const handleTikTokCommand = async (senderId, args, pageAccessToken) => {
-  if (args.length === 0) {
-    return sendMessage(senderId, { text: 'Please provide a TikTok video URL' }, pageAccessToken);
-  }
-
-  const tiktokUrl = args[0];
-  getTikTokVideo(tiktokUrl).then((videoUrl) => {
-    if (videoUrl) {
-      sendMessage(senderId, { text: `TikTok video URL: ${videoUrl}` }, pageAccessToken);
-    } else {
-      sendMessage(senderId, { text: 'Failed to retrieve TikTok video URL' }, pageAccessToken);
-    }
-  });
-};
-
 module.exports = {
-  name: 'tiktokdl',
-  description: 'Get TikTok video URL',
-  usage: 'tiktokdl <TikTok video URL>',
-  author: 'Raniel',
-  execute: handleTikTokCommand,
+  name: 'tiktok',
+  description: 'Download TikTok video',
+  usage: 'tiktok <video link>',
+  author: 'raniel',
+  execute: async (senderId, args) => {
+    const pageAccessToken = token;
+    const videoLink = args.join(' ');
+    if (!videoLink) {
+      return sendMessage(senderId, { text: 'Usage: tiktok <video link>' }, pageAccessToken);
+    }
+    try {
+      const response = await axios.get(`https://sandipbaruwal.onrender.com/tikdown?url=${encodeURIComponent(videoLink)}`);
+      const videoData = response.data;
+      const videoUrl = videoData;
+      if (!videoUrl) {
+        return sendMessage(senderId, { text: 'Failed to retrieve video.' }, pageAccessToken);
+      }
+      sendMessage(senderId, { attachment: { type: 'video', payload: { url: videoUrl } } }, pageAccessToken);
+    } catch (error) {
+      console.error('Error:', error.message);
+      sendMessage(senderId, { text: 'An error occurred. Try again later.' }, pageAccessToken);
+    }
+  }
 };
