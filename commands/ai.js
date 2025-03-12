1b4821416300ccade9b4bdd32ec13c83aea707e6
Bogiti <https://github.com/geliemadason/Raniel-Page-Bot.git>
2025-03-12 19:37:17 +0800

    update ai.js

---
+++ commands/ai.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+const { sendMessage } = require('../handles/sendMessage');
+
+module.exports = {
+  name: 'ai',
+  description: 'Interact with GPT-4o',
+  usage: 'gpt4 [your message]',
+  author: 'coffee',
+
+  async execute(senderId, args, pageAccessToken) {
+    const prompt = args.join(' ');
+    if (!prompt) return sendMessage(senderId, { text: "Usage: gpt4 <question>" }, pageAccessToken);
+
+    try {
+      const { data: { response } } = await axios.get(`https://zen-api.up.railway.app/api/gpt4?prompt=${encodeURIComponent(prompt)}&uid=${senderId}`);
+
+      const parts = [];
+
+      for (let i = 0; i < response.length; i += 1999) {
+        parts.push(response.substring(i, i + 1999));
+      }
+
+      // send all msg parts
+      for (const part of parts) {
+        await sendMessage(senderId, { text: part }, pageAccessToken, ' Sending...');
+      }
+
+    } catch {
+      sendMessage(senderId, { text: 'There was an error generating the content. Please try again later.' }, pageAccessToken);
+    }
+  }
+};
