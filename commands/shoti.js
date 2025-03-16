773e05a3c659d583572f5ee9db724f64cbe82675
Bogiti <https://github.com/geliemadason/Raniel-Page-Bot.git>
2025-03-16 14:12:47 +0800

    update shoti.js

--- commands/shoti.js
+++ commands/shoti.js
@@ -3,16 +3,15 @@ const { sendMessage } = require('../handles/sendMessage');
 
 module.exports = {
   name: 'shoti',
-  description: 'Generate a random TikTok video.',
-  author: 'Jerome',
-
+  description: 'Generate a random video.',
+  author: 'Raniel',
   async execute(senderId, args, pageAccessToken) {
     try {
       const apiUrl = 'https://kaiz-apis.gleeze.com/api/shoti';
       const response = await axios.get(apiUrl);
 
-      if (response.data && response.data.videoDownloadLink) {
-        const videoUrl = response.data.videoDownloadLink;
+      if (response.data && response.data.video_url) {
+        const videoUrl = response.data.video_url;
 
         // Send video attachment only
         const videoMessage = {
@@ -23,17 +22,14 @@ module.exports = {
             },
           },
         };
+
         await sendMessage(senderId, videoMessage, pageAccessToken);
       } else {
-        await sendMessage(senderId, {
-          text: '⚠️ No video found. Please try again later.',
-        }, pageAccessToken);
+        await sendMessage(senderId, { text: 'No video found. Please try again later.' }, pageAccessToken);
       }
     } catch (error) {
-      console.error('Error fetching TikTok video:', error.message);
-      await sendMessage(senderId, {
-        text: '⚠️ Sorry, there was an error generating the video. Please try again later.',
-      }, pageAccessToken);
+      console.error('Error fetching video:', error.message);
+      await sendMessage(senderId, { text: 'Sorry, there was an error generating the video. Please try again later.' }, pageAccessToken);
     }
   },
 };
