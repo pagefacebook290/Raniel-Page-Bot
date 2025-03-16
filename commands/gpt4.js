1bf47866058b0dccf1be8286ba8f22ff8d7b3758
Bogiti <https://github.com/geliemadason/Raniel-Page-Bot.git>
2025-03-16 22:47:21 +0800

    update gpt4.js

--- commands/gpt4.js
+++ commands/gpt4.js
@@ -12,7 +12,7 @@ module.exports = {
     if (!prompt) return sendMessage(senderId, { text: "Usage: gpt4 <question>" }, pageAccessToken);
 
     try {
-      const { data: { response } } = await axios.get(`https://kaiz-apis.gleeze.com/api/gpt-3.5?q=${encodeURIComponent(prompt)}`);
+      const { data: { response } } = await axios.get(`https://zen-api.up.railway.app/api/gpt4?prompt=${encodeURIComponent(prompt)}&uid=${senderId}`);
 
       const parts = [];
 
