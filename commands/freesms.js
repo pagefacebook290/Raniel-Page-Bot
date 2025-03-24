ac0c1590b4f550d693a83b239f8ea17f637cf239
Bogiti <https://github.com/geliemadason/Raniel-Page-Bot.git>
2025-03-25 07:02:50 +0800

    update freesms.js

--- commands/freesms.js
+++ commands/freesms.js
@@ -3,24 +3,22 @@ const axios = require('axios');
 module.exports = {
   name: 'freesms',
   description: 'freesms <phonenumber> <message>.',
-  author: 'Raniel',
+  author: 'raniel',
   
   async execute(senderid, args, pageaccesstoken, sendmessage) {
     const phonenumber = args[0];
     const message = args.slice(1).join(' ');
 
     if (!phonenumber || !message) {
-      return sendmessage(senderid, { text: 'freesms phonenumber message.' }, pageaccesstoken);
+      return sendmessage(senderid, { text: '❌ Usage: freesms <phonenumber> <message>.' }, pageaccesstoken);
     }
 
-    sendmessage(senderid, { text: '⏳ 𝗣𝗿𝗼𝗰𝗲𝘀𝘀𝗶𝗻𝗴 𝘆𝗼𝘂𝗿 𝗿𝗲𝗾𝘂𝗲𝘀𝘁 𝘁𝗼 𝘀𝗲𝗻𝗱 𝘀𝗺𝘀, 𝗽𝗹𝗲𝗮𝘀𝗲 𝘄𝗮𝗶𝘁...' }, pageaccesstoken);
+    sendmessage(senderid, { text: '⏳ Processing your request to send SMS, please wait...' }, pageaccesstoken);
 
     try {
-      const response = await axios.get('https://haji-mix.up.railway.app/api/lbcsms', {
-        params: {
-          number: phonenumber,
-          message: encodeURIComponent(message)
-        }
+      const response = await axios.post('https://haji-mix.up.railway.app/api/lbcsms', {
+        number: phonenumber,
+        message: encodeURIComponent(message) // Correctly encode the message
       });
 
       const { status, response: messageresponse } = response.data;
@@ -29,17 +27,15 @@ module.exports = {
 
       if (status) {
         sendmessage(senderid, { 
-          text: `𝗠𝗲𝘀𝘀𝗮𝗴𝗲 𝗵𝗮𝘀 𝗯𝗲𝗲𝗻 𝘀𝘂𝗰𝗰𝗲𝘀𝘀𝗳𝘂𝗹𝗹𝘆 𝘀𝗲𝗻𝘁 ✅ \n\n📬 𝗠𝗲𝘀𝘀𝗮𝗴𝗲: ${messageresponse}\n\n⏰ 𝗔𝘀𝗶𝗮/𝗠𝗮𝗻𝗶𝗹𝗮: ${responsetime}`
+          text: `✅ Message has been successfully sent! \n\n📬 Message: ${messageresponse}\n\n⏰ Asia/Manila: ${responsetime}`
         }, pageaccesstoken);
       } else {
-        sendmessage(senderid, { text: `☑𝗠𝗲𝘀𝘀𝗮𝗴𝗲 𝗵𝗮𝘀 𝗯𝗲𝗲𝗻 𝘀𝘂𝗰𝗰𝗲𝘀𝘀𝗳𝘂𝗹𝗹𝘆 𝘀𝗲𝗻𝘁 ✅ \n\n📬 𝗠𝗲𝘀𝘀𝗮𝗴𝗲: ${messageresponse}\n\n⏰ 𝗔𝘀𝗶𝗮/𝗠𝗮𝗻𝗶𝗹𝗮: ${responsetime}`
-          
-        }, pageaccesstoken);
+        sendmessage(senderid, { text: `❌ Failed to send the message: ${messageresponse}` }, pageaccesstoken);
       }
 
     } catch (error) {
-      console.error('error:', error);
-      sendmessage(senderid, { text: '❌ 𝗙𝗮𝗶𝗹𝗲𝗱 𝘁𝗼 𝘀𝗲𝗻𝗱 𝘁h𝗵𝗺𝗲𝘀𝘀𝗮𝗴𝗲.' }, pageaccesstoken);
+      console.error('Error:', error);
+      sendmessage(senderid, { text: '❌ Failed to send the message.' }, pageaccesstoken);
     }
   }
 };
