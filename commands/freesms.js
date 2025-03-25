3efee0927abce4f6ee1efe27c21b19e8d5b8ec3e
Bogiti <https://github.com/geliemadason/Raniel-Page-Bot.git>
2025-03-25 09:37:00 +0800

    update freesms.js

--- commands/freesms.js
+++ commands/freesms.js
@@ -10,13 +10,14 @@ module.exports = {
     const message = args.slice(1).join(' ');
 
     if (!phonenumber || !message) {
-      return sendmessage(senderid, { text: 'freesms phonenumber message.' }, pageaccesstoken);
+      return sendmessage(senderid, { text: `❌ 𝗨𝘀𝗮𝗴𝗲: freesms <phonenumber> <message>.` }, 
+      pageaccesstoken);
     }
 
     sendmessage(senderid, { text: '⏳ 𝗣𝗿𝗼𝗰𝗲𝘀𝘀𝗶𝗻𝗴 𝘆𝗼𝘂𝗿 𝗿𝗲𝗾𝘂𝗲𝘀𝘁 𝘁𝗼 𝘀𝗲𝗻𝗱 𝘀𝗺𝘀, 𝗽𝗹𝗲𝗮𝘀𝗲 𝘄𝗮𝗶𝘁...' }, pageaccesstoken);
 
     try {
-      const response = await axios.get('', {
+      const response = await axios.get('https://wiegines3.vercel.app/api/freesms', {
         params: {
           number: phonenumber,
           message: encodeURIComponent(message)
@@ -32,14 +33,13 @@ module.exports = {
           text: `𝗠𝗲𝘀𝘀𝗮𝗴𝗲 𝗵𝗮𝘀 𝗯𝗲𝗲𝗻 𝘀𝘂𝗰𝗰𝗲𝘀𝘀𝗳𝘂𝗹𝗹𝘆 𝘀𝗲𝗻𝘁 ✅ \n\n📬 𝗠𝗲𝘀𝘀𝗮𝗴𝗲: ${messageresponse}\n\n⏰ 𝗔𝘀𝗶𝗮/𝗠𝗮𝗻𝗶𝗹𝗮: ${responsetime}`
         }, pageaccesstoken);
       } else {
-        sendmessage(senderid, { text: `☑𝗠𝗲𝘀𝘀𝗮𝗴𝗲 𝗵𝗮𝘀 𝗯𝗲𝗲𝗻 𝘀𝘂𝗰𝗰𝗲𝘀𝘀𝗳𝘂𝗹𝗹𝘆 𝘀𝗲𝗻𝘁 ✅ \n\n📬 𝗠𝗲𝘀𝘀𝗮𝗴𝗲: ${messageresponse}\n\n⏰ 𝗔𝘀𝗶𝗮/𝗠𝗮𝗻𝗶𝗹𝗮: ${responsetime}`
-          
+        sendmessage(senderid, { text: `𝗠𝗲𝘀𝘀𝗮𝗴𝗲 𝗵𝗮𝘀 𝗯𝗲𝗲𝗻 𝘀𝘂𝗰𝗰𝗲𝘀𝘀𝗳𝘂𝗹𝗹𝘆 𝘀𝗲𝗻𝘁 ✅ \n\n📬 𝗠𝗲𝘀𝘀𝗮𝗴𝗲: ${messageresponse}\n\n⏰ 𝗔𝘀𝗶𝗮/𝗠𝗮𝗻𝗶𝗹𝗮: ${responsetime}`
         }, pageaccesstoken);
       }
 
     } catch (error) {
       console.error('error:', error);
-      sendmessage(senderid, { text: '❌ 𝗙𝗮𝗶𝗹𝗲𝗱 𝘁𝗼 𝘀𝗲𝗻𝗱 𝘁h𝗵𝗺𝗲𝘀𝘀𝗮𝗴𝗲.' }, pageaccesstoken);
+      sendmessage(senderid, { text: '❌ 𝗙𝗮𝗶𝗹𝗲𝗱 𝘁𝗼 𝘀𝗲𝗻𝗱 𝘁𝗵𝗲 𝗺𝗲𝘀𝘀𝗮𝗴𝗲.' }, pageaccesstoken);
     }
   }
 };
