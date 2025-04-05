d22620f19d0215a17b31697f4db5b698ed6c3cc0
Bogiti <https://github.com/geliemadason/Raniel-Page-Bot.git>
2025-04-05 09:54:40 +0800

    update shoti.js

--- commands/shoti.js
+++ commands/shoti.js
@@ -4,6 +4,7 @@ const path = require("path");
 
 module.exports = {
   name: "shoti",
+  usePrefix: false,
   usage: "shoti",
   version: "1.0",
   execute: async ({ api, event }) => {
@@ -64,38 +65,3 @@ module.exports = {
   },
 };
 
-const axios = require("axios");
-const { sendMessage } = require("../handles/sendMessage");
-
-module.exports = {
-  name: "billboard",
-  description: "generates a billboard image based on a prompt",
-  usage: "lexi [prompt]",
-  author: "Gelie",
-  async execute(senderId, args, pageAccessToken) {
-    if (!args || args.length === 0) {
-      await sendMessage(senderId, {
-        text: "you need to provide prompt stupid idiot.",
-      }, pageAccessToken);
-      return;
-    }
-    const prompt = args.join(" ");
-    const apiUrl = `https://api-canvass.vercel.app/billboard?text=${encodeURIComponent(prompt)}`;
-    await sendMessage(senderId, { text: "⌛Sending your damn image, bitch..." }, pageAccessToken);
-    try {
-      await sendMessage(senderId, {
-        attachment: {
-          type: "image",
-          payload: {
-            url: apiUrl,
-          },
-        },
-      }, pageAccessToken);
-    } catch (error) {
-      console.error("Shit, something went wrong. Fucking API screwed up.", error);
-      await sendMessage(senderId, {
-        text: "An error occurred while generating the image. Please try again later.",
-      }, pageAccessToken);
-    }
-  },
-};
