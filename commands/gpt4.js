1b669a0b68b30a08e94702ddb5361106f0c22664
gelie <https://github.com/geliemadason/Fb-Autoshare-modified-by-raniel.git>
2025-05-12 07:09:39 +0800

    update gpt4.js

--- commands/gpt4.js
+++ commands/gpt4.js
@@ -13,7 +13,7 @@ module.exports = {
     if (!prompt) return sendMessage(senderId, { text: "Usage: gpt4 <question>" }, pageAccessToken);
 
     try {
-      const { data: { response } } = await axios.get(`https://kaiz-apis.gleeze.com/api/wizardLM2-8x22b?ask=${encodeURIComponent(prompt)}&uid=${senderId}`);
+      const { data: { response } } = await axios.get(`https://kaiz-apis.gleeze.com/api/you-ai?ask=${encodeURIComponent(prompt)}&uid=${senderId}`);
 
       const parts = [];
 
