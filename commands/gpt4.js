c52c6169d8458cbc16e236a2e7f301d9ab8c8cf2
geliemadason@gmail.com <https://github.com/geliemadason/Raniel-Page-Bot.git>
2025-05-23 15:08:26 +0800

    update gpt4.js

--- commands/gpt4.js
+++ commands/gpt4.js
@@ -13,7 +13,7 @@ module.exports = {
     if (!prompt) return sendMessage(senderId, { text: "Usage: gpt4 <question>" }, pageAccessToken);
 
     try {
-      const { data: { response } } = await axios.get(`https://kaiz-apis.gleeze.com/api/gpt-4o?ask=${encodeURIComponent(prompt)}&uid=${senderId}&webSearch=off&apikey=3be7c751-a15e-4ff7-8896-72cfdfc811ac`);
+      const { data: { response } } = await axios.get(`https://kaiz-apis.gleeze.com/api/gpt-4o?ask=${encodeURIComponent(prompt)}&uid=${senderId}&webSearch=off&apikey=1301192b-ce75-4de8-a8ff-6457ba456f78`);
 
       const parts = [];
 
